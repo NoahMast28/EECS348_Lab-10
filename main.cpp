e5e646e05ef6fcd1952eef03b5b53d77dd371651
Noah Mast <noahmast28@gmail.com>
2025-04-16 23:12:01 -0500

    Fixed missing semicolon

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
             std::string result = add_string_doubles(line); // Adds the double with the double specified in the lab instructions
             std::cout << line << " + (-123.456) = " << result << std::endl; // Prints the result
         } else {
-            std::cout << line << " is not a valid double" << std::endl // Informs the user that the line did not contain a vaild double
+            std::cout << line << " is not a valid double" << std::endl; // Informs the user that the line did not contain a vaild double
         }
     }
 
