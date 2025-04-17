95b30effb3c385cbf3c2ca44402ff391aa22ab89
Noah Mast <noahmast28@gmail.com>
2025-04-16 19:38:24 -0500

    Added is_num() and is_valid_double() to function declarations

--- calculator.hpp
+++ calculator.hpp
@@ -7,4 +7,7 @@
 // validated and thus throw no exceptions
 double parse_number(const std::string &expression);
 
+bool is_valid_double(const std::string &line);
+bool is_num(char c);
+
 #endif // __CALCULATOR_HPP
