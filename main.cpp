2ebf56611d256cc0710014052e220b6ce77b8b20
Noah Mast <noahmast28@gmail.com>
2025-04-16 19:37:27 -0500

    Implemented file parsing and result printing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 #include "calculator.hpp"
 
 int main(int argc, char *argv[]) {
+    // Tells the user to add a filename when running the file
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+        return 1;
+    }
 
+    std::ifstream file(argv[1]); // Opens the file
+
+    std::string line;
+
+    while (std::getline(file, line)) { // Iterates over each line of the file
+        if (is_valid_double(line)) { // Checks if the line contains a valid double number
+            std::string result = parse_number(line); // Adds the double with the double specified in the lab instructions
+            std::cout << line << " + (-123.456) = " << result << std::endl; // Prints the result
+        } else {
+            std::cout << line << " is not a valid double" << std::endl // Informs the user that the line did not contain a vaild double
+        }
+    }
+
+    file.close(); // Closes the file
+
+    return 0;
 }
